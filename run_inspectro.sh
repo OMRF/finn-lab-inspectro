4ee86442090e036e34444c4a6aeb917ce433a7f6
Rikki Adams <rikki.ada5500@gmail.com>
2026-07-10 16:00:26 +0000

    name job for future reference

--- run_inspectro.sh
+++ run_inspectro.sh
@@ -1,3 +1,3 @@
 ml inspectro
 ml slurm
-sbatch --cpus-per-task 20 --mem 248G --wrap 'snakemake --cores all --snakefile Snakefile'
+sbatch --job-name inspectro --cpus-per-task 20 --mem 248G --wrap 'snakemake --cores all --snakefile Snakefile'

f7d36f33863b43cde45c31878ae8231bd577dab1
Christopher Bottoms, PhD <Christopher-Bottoms@omrf.org>
2026-07-10 14:11:13 +0000

    clean up code

--- heatmap_histonemods.R
+++ heatmap_histonemods.R
@@ -18,54 +18,56 @@ mod_name <- args[4]
 clusterbasename <- basename(cluster_file)
 base_out_name <- strsplit(clusterbasename,"\\.") [[1]][1]
 
-print("we made it to 21")
+print("DEBUG: we made it to 21")
 
+# Use rtracklayer's import function to read in a BigWig file
 bw <- import(format = "BigWig", con = bigwig_file)
 
+
+# Create 50kb bins
 bins_50kb <- tileGenome(seqlengths(bw), tilewidth = 50000, cut.last.tile.in.chrom=TRUE)
 
+# squish the results into 50kb bins
 bin_result <- binnedAverage(bins_50kb, numvar = coverage(bw, weight = "score"), varname = "avg_score")
-print("we made it to 28!!")
 
+print("DEBUG: we made it to 28!!")
+
+# convert binned result into a data frame
 data_ranges_df <- bin_result %>% as.data.frame()
 
 df <- read.csv(eigvecs_file)
 
+# Get kmeans_sm9 cluster labels
 clust <- read.table(cluster_file, header=TRUE)
 k9 <- clust[, c("chrom", "start", "end", "kmeans_sm9") ]
+
+# Add kmeans_sm9 cluster labels to eigenvecs info
 combo <- merge(df, k9)
 
+# Leave out most of the Eigenvector columns
 small <- combo[,c("chrom","start","end","GC","arm","armlen","centel","centel_abs","is_bad","kmeans_sm9","E0","E1","E2","E3","E4","E5","E6","E7","E8","E9","E10")]
-longer <- small %>% pivot_longer(cols=c("E0","E1","E2","E3","E4","E5","E6","E7","E8","E9","E10"), values_to = "value")
-bin_result
-head(data_ranges_df)
-longer[1:20, ]
-longer[1:40, ]
-longer[30:40, ]
-longer[3010:3040, ]
-longer2 <- longer
-longer2 <- longer %>% mutate(start = start +1 )
-longer2
-big_combo <- merge(longer2, data_ranges_df)
-ls()
-head(small)
+
+# Convert coordinates to 1-based (instead of 0-based)
 small2 <- small %>% mutate(start = start +1)
-big_combo <- merge(small2, data_ranges_df)
-head(big_combo)
-head(data_ranges_df)
+
+# add column named "chrom" that replicates the seqnames column
 data_ranges_df2 <- data_ranges_df %>% mutate(chrom = seqnames)
-print("we made it to 57!!")
-# Did we want all?
-big_combo <- merge(small2, data_ranges_df2, all = TRUE)
+
+print("DEBUG: we made it to 57!!")
+
+#big_combo <- merge(small2, data_ranges_df2, all = TRUE) # Did we want all?
 big_combo <- merge(small2, data_ranges_df2, all = FALSE)
 
 plot_Histone <- ggplot(big_combo, aes(x=centel, y=1, color = avg_score)) +
                 geom_tile() +
                 scale_color_viridis_c() +
                 facet_grid(.~kmeans_sm9)
-print("we made it to 66!!")
+
+print("DEBUG: we made it to 66!!")
+
 pdf_filename <-paste0(base_out_name,".", mod_name,".kmeanssm9.pdf")
 pdf(pdf_filename, width =18, height =2)
 print(plot_Histone)
 dev.off()
-print("we finished!!")
+
+print("DEBUG: we finished!!")
